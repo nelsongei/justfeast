3a993fde25922b3c1d7ff5f92753ef16f3e8f6f7
nelsongei <psammy.ngei@gmail.com>
2026-08-31 15:28:21 +0300

    updates

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -553,7 +553,7 @@ function escapeHtmlUser(str) {
   return String(str)
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
-    .replace/>/g, '&gt;')
+    .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&#039;');
 }
